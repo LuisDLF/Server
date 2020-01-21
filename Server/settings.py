d3113267693d7cde38d5c086990c402dea4ec68c
Eduardo Uriel Garza Gonzalez <eduardo.garza.gzz@outlook.es>
2020-01-21 13:16:48 -0600

    Test 005

--- Server/settings.py
+++ Server/settings.py
@@ -135,6 +135,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-STATICFILES_DIRS = (os.path.join(SITE_ROOT, 'static/'),)
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
